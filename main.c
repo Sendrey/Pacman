92b7309ed49c1b1e33e394f7a0b46bef5b74eefd
ValentinHamon <116344856+ValentinHamon@users.noreply.github.com>
2024-01-30 17:03:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,29 @@
 #include <test>
 BAPTISTE EST UN GRAND FOUFOU
+int pac[10][10]={
+	{1,1,1,1,1,1,1,1,1,1},
+	{1,2,2,2,2,2,2,2,2,1},
+	{1,2,1,1,2,1,2,1,2,1},
+	{1,2,1,1,2,1,2,1,2,1},
+	{1,2,2,2,2,2,2,2,2,1},
+	{1,1,2,1,1,1,1,2,1,1},
+	{1,1,2,1,1,1,1,2,1,1},
+	{1,2,2,2,1,1,2,2,2,1},
+	{1,2,1,2,1,1,2,1,2,1},
+	{1,2,2,2,1,1,2,2,2,1},
+};
+
+
+	
+void plateau(int pac[10][10]){
+	int i,j;
+	for(i=0;i<HAUT;i=i+1)
+		for(j=0;j<LARG;j=j+1)
+			if(pac[i][j]==1){
+				renderTexture(wall, ren, i*LARG, j*HAUT, LARG, HAUT);
+			}
+			if(pac[i][j]==2){
+				renderTexture(gum, ren, i*LARG, j*HAUT, LARG, HAUT);
+			}
+}
+				
